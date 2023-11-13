2c749fe0a0d69b0a161575784476d1a0fcef4359
Matteo Falasca <matteo.falasca@emmek.org>
2023-11-13 11:37:45 +0100

    first commit

---
+++ src/test/java/org/emmek/beu2w3d1/U5d11ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.emmek.beu2w3d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
